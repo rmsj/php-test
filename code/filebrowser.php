cf8cc883213b4390129b09f99ce82ce4b0d01a82
Ronaldo Santana <ronaldo.santana@myeasyweb.co>
2018-11-13 21:35:07 +1300

    initial implementation of interface for filebrowser class

--- code/filebrowser.php
+++ code/filebrowser.php
@@ -3,4 +3,55 @@ require_once('interface.php');
 
 class FileBrowser implements __FileBrowser {
 
+    /**
+     * @var string
+     */
+    private $rootPath;
+
+    /**
+     * @var string
+     */
+    private $currentPath;
+
+    /**
+     * @var array
+     */
+    private $extensionFilter;
+
+    public function __construct($rootPath, $currentPath = null, array $extensionFilter = array())
+    {
+        $this->SetRootPath($rootPath);
+        if ($currentPath) {
+            $this->SetCurrentPath($currentPath);
+        }
+        if ($extensionFilter) {
+            $this->SetExtensionFilter($extensionFilter);
+        }
+    }
+
+    public function SetRootPath($rootPath)
+    {
+        $this->rootPath = $rootPath;
+    }
+
+    /**
+     * @param $currentPath
+     */
+    public function SetCurrentPath($currentPath)
+    {
+        $this->currentPath = $currentPath;
+    }
+
+    /**
+     * @param array $extensionFilter
+     */
+    public function SetExtensionFilter(array $extensionFilter)
+    {
+        $this->extensionFilter = $extensionFilter;
+    }
+
+    public function Get()
+    {
+        return [];
+    }
 }
